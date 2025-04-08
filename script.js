194866ecf0450a4f5b7d209ecb546d792e8e4a3b
Himanshu Kumar <himanshu7277069@gmail.com>
2025-04-08 09:40:58 +0530

    small changes

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-let hambuger = document.querySelector('.hamburger');
-let times = document.querySelector('.times');
-let mobileNav = document.querySelector('.mobile-nav');
+let hamburger = document.querySelector(".hamburger");
+let times = document.querySelector(".times");
+let mobileNav = document.querySelector(".mobile-nav");
+let navLinks = document.querySelectorAll(".mobile-nav a"); 
 
-hambuger.addEventListener('click', function () {
-    mobileNav.classList.add('open');
+hamburger.addEventListener("click", function () {
+	mobileNav.classList.add("open");
 });
 
-times.addEventListener('click', function () {
-    mobileNav.classList.remove('open');
+times.addEventListener("click", function () {
+	mobileNav.classList.remove("open");
+});
+
+navLinks.forEach((link) => {
+	link.addEventListener("click", function () {
+		mobileNav.classList.remove("open");
+	});
 });
